a0377455b101b279a514903005e6d63507948aac
JonathanSaudhof <code@jonathansaudhof.de>
2024-10-21 20:57:33 +0200

    feat: added three split

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,37 +8,37 @@ export default function Home() {
   const [alpha, setAlpha] = useState(ALPHABET);
   const [word, setWord] = useState("");
 
-  const mid = useMemo(() => Math.round(alpha.length / 2), [alpha]);
-
   const addToWord = (char: string) => {
     setWord((prev) => prev + char);
     setAlpha(ALPHABET);
   };
 
-  const handleLeft = () => {
-    const nextAlpha = alpha.slice(0, mid);
-    console.log(nextAlpha);
-    if (nextAlpha.length === 1) {
-      addToWord(nextAlpha);
+  const handleLeft = (newAlpha: string) => {
+    console.log(newAlpha);
+    if (newAlpha.length === 1) {
+      addToWord(newAlpha);
       return;
     }
-    setAlpha(() => nextAlpha);
-  };
-
-  const handleRight = () => {
-    const nextAlpha = alpha.slice(mid);
-    if (nextAlpha.length === 1) {
-      addToWord(nextAlpha);
-      return;
-    }
-
-    setAlpha(() => nextAlpha);
+    setAlpha(() => newAlpha);
   };
 
   const reset = () => {
     setWord("");
     setAlpha(ALPHABET);
   };
+
+  function getAlpha(alphaSlice: string): string[] {
+    const oneThird = Math.ceil(alphaSlice.length / 3);
+    const twoThird = Math.ceil((alphaSlice.length / 3) * 2);
+
+    const left = alphaSlice.slice(0, oneThird);
+    const mid = alphaSlice.slice(oneThird, twoThird);
+    const right = alphaSlice.slice(twoThird);
+
+    return [left, mid, right];
+  }
+
+  const buttonSlice = useMemo(() => getAlpha(alpha), [alpha]);
 
   return (
     <main className="flex flex-col gap-8 mx-auto container min-h-svh py-12">
@@ -47,10 +47,23 @@ export default function Home() {
         <span className="uppercase text-6xl text-center underline">{word}</span>
       </div>
       <div className="flex justify-evenly gap-4 flex-1">
-        <Button onClick={handleLeft}>{alpha.slice(0, mid).split("")}</Button>
-        {alpha.length > 1 ? (
-          <Button onClick={handleRight}>{alpha.slice(mid)}</Button>
-        ) : null}
+        {buttonSlice.map((slice) => {
+          if (slice.length === 0) {
+            return null;
+          }
+          return (
+            <Button key={slice} onClick={() => handleLeft(slice)}>
+              {slice.split("").map((char) => (
+                <span
+                  key={char}
+                  className="h-24 w-24 flex items-center justify-center"
+                >
+                  {char}
+                </span>
+              ))}
+            </Button>
+          );
+        })}
       </div>
       <div className="w-full flex">
         <Button onClick={reset}>Zurücksetzen</Button>
@@ -64,7 +77,7 @@ const Button = ({
   onClick,
 }: Readonly<PropsWithChildren<{ onClick: () => void }>>) => (
   <button
-    className="px-4 py-2 text-gray-900 border-4 border-gray-900 text-5xl uppercase rounded-xl flex-1"
+    className="px-4 py-2 text-gray-900 border-4 border-gray-900 text-5xl uppercase rounded-xl flex-1 flex flex-wrap justify-center items-center"
     onClick={onClick}
   >
     {children}
