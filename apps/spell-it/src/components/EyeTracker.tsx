a54b3e7ddbde995ca250268d3ff1ef093a6b2cd2
JonathanSaudhof <code@jonathansaudhof.de>
2024-11-02 14:23:11 +0100

    chore: removed plotting_canvas

--- apps/spell-it/src/components/EyeTracker.tsx
+++ apps/spell-it/src/components/EyeTracker.tsx
@@ -27,38 +27,6 @@ export default function EyeTracker({ children }: Readonly<PropsWithChildren>) {
 
   const isFeatureActive = env.NEXT_PUBLIC_FEAT_EYE_TRACKER;
   const isDevelopment = env.NEXT_PUBLIC_ENV === "development";
-  const setup = function () {
-    //Set up the main canvas. The main canvas is used to calibrate the webgazer.
-    const canvas = document.getElementById(
-      "plotting_canvas"
-    ) as HTMLCanvasElement;
-
-    if (!canvas) {
-      console.error("Canvas not found");
-      return;
-    }
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    canvas.style.position = "fixed";
-  };
-
-  useEffect(() => {
-    function resize() {
-      const canvas = document.getElementById(
-        "plotting_canvas"
-      ) as HTMLCanvasElement;
-      const context = canvas.getContext("2d");
-      if (!context) {
-        return;
-      }
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-    }
-    window.addEventListener("resize", resize, false);
-
-    return () => window.removeEventListener("resize", resize, false);
-  }, []);
 
   useEffect(() => {
     if (typeof window !== "undefined" && isFeatureActive) {
@@ -74,7 +42,6 @@ export default function EyeTracker({ children }: Readonly<PropsWithChildren>) {
             console.log("Webgazer instance", instance);
             setWebgazer(instance);
             console.log("Webgazer started");
-            setup();
             setIsLoaded(true);
           })
       );
@@ -90,7 +57,6 @@ export default function EyeTracker({ children }: Readonly<PropsWithChildren>) {
 
   return (
     <EyeTrackerContext.Provider value={contextValue}>
-      <canvas id="plotting_canvas" />
       <div className="relative">
         {!isLoaded && isFeatureActive ? (
           <div className="fixed bg-white flex justify-center items-center py-4 w-full z-50 shadow">
